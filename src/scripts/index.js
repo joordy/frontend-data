c7f348f517a5bc86b6d96d8b72cf28e29475e5f6
joordy <jmfronik@gmail.com>
2020-11-06 14:57:49 +0100

    Creating cleaned-up data from arrays

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,28 @@
 import 'regenerator-runtime/runtime';
 import './../scss/main.scss';
 // importing Utils
-import { makeMap } from './utils/visuals/map';
+// import { makeMap } from './utils/visuals/map';
+// import { locationMap } from './utils/visuals/locationMap';
 import { endpoint1, endpoint2 } from './utils/config/endPoints';
 import { getData } from './utils/config/getApi';
-// import { getAreaManager } from './utils/filters/data';
+import { capacityFilter, evCapacityFilter, maxVehicleFilter, garageLocations } from './utils/filters/receivingData';
 
 // let selectedColumn = null;
 
 getData(endpoint1, endpoint2).then((rdwData) => {
   console.log(rdwData);
 
+  const carCapacity = capacityFilter(rdwData);
+  const evChargers = evCapacityFilter(rdwData);
+  const maxDriveThrough = maxVehicleFilter(rdwData);
+  const locations = garageLocations(rdwData);
+  console.log('shows capacity', carCapacity);
+  console.log('shows ev-chargers', evChargers);
+  console.log('shows drive-height', maxDriveThrough);
+  console.log('shows locations', locations);
+
+  //locationMap(locations);
+  // console.log(filterCapacity);
   // function checkIfTrue() {
   //   rdwData.forEach((parkingSpecs) => {
   //     if (typeof parkingSpecs != undefined) {
@@ -20,25 +32,11 @@ getData(endpoint1, endpoint2).then((rdwData) => {
   // }
   // checkIfTrue();
 
-  let filteredData = rdwData.map((item) => {
-    return {
-      itemDesc: item.areadesc,
-      itemID: item.areaid,
-      long: item.location.longitude,
-      parking: item.parkingSpecs.areaid,
-
-      // parkingEV: item.parkingSpecs.chargingpointcapacity,
-      // parking: item.parkingSpecs, // returned heel object
-      // parkingCap: item.parkingSpecs, // onbereikbaar
-    };
-  });
-
-  console.log(filteredData);
-
   // console.log(rdwData);
   // selectedColumn = 'areamanagerid';
   // const areaManagersID = getAreaManager(rdwData, selectedColumn);
   // console.log(areaManagersID);
 
-  makeMap();
+  // makeBar(carCapacity);
+  //makeMap();
 });
---
+++ src/scripts/utils/filters/receivingData.js
@@ -0,0 +1,50 @@
+// export function getAreaManager(dataArray, key) {
+//   return dataArray.map((item) => item[key]);
+// }
+
+export let capacityFilter = (rdwData) => {
+  return rdwData.map((item) => {
+    return {
+      itemDesc: item.areadesc,
+      itemID: item.areaid,
+      carCapacity: parseInt(item.parkingSpecs && item.parkingSpecs.capacity ? item.parkingSpecs.capacity : undefined),
+    };
+  });
+};
+
+export let evCapacityFilter = (rdwData) => {
+  return rdwData.map((item) => {
+    return {
+      itemDesc: item.areadesc,
+      itemID: item.areaid,
+      evChargerCapacity: parseInt(
+        item.parkingSpecs && item.parkingSpecs.chargingpointcapacity
+          ? item.parkingSpecs.chargingpointcapacity
+          : undefined
+      ),
+    };
+  });
+};
+
+export let maxVehicleFilter = (rdwData) => {
+  return rdwData.map((item) => {
+    return {
+      itemDesc: item.areadesc,
+      itemID: item.areaid,
+      maxDriveThrough: parseInt(
+        item.parkingSpecs && item.parkingSpecs.maximumvehicleheight ? item.parkingSpecs.maximumvehicleheight : undefined
+      ),
+    };
+  });
+};
+
+export let garageLocations = (rdwData) => {
+  return rdwData.map((item) => {
+    return {
+      itemDesc: item.areadesc,
+      itemID: item.areaid,
+      lat: item.location.latitude,
+      long: item.location.longitude,
+    };
+  });
+};
